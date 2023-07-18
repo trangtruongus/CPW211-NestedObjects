20ec15210f5865907cfb31fef31e7b53b928c21b
trangtruongus <trang.truong.us@icloud.com>
2023-07-17 22:35:47 -0500

    Added missing documentation and student date of birth

--- CPW211-NestedObjects/Student.cs
+++ CPW211-NestedObjects/Student.cs
@@ -26,8 +26,19 @@ namespace NestedObjects
         /// </summary>
         public string SchoolEmail { get; set; }
 
+        /// <summary>
+        /// The date of birth
+        /// </summary>
+        public DateOnly DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Students preferred contact number
+        /// </summary>
         public string PhoneNumber { get; set; }
 
+        /// <summary>
+        /// The advisor who is assigned to the student
+        /// </summary>
         public Advisor AssignedAdvisor { get; set; }
     }
 }
