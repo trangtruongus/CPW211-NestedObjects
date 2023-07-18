e9eb8ee4f09aae7cf139fc37d5fb4724d25da5f2
trangtruongus <trang.truong.us@icloud.com>
2023-07-17 22:37:13 -0500

    Added test code

--- CPW211-NestedObjects/Program.cs
+++ CPW211-NestedObjects/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NestedObjects;
+
+Advisor compSciAdvisor = new()
+{
+    Email = "Annemarie@cptc.edu",
+    FullName = "Annemarie Smith",
+    OfficeLocation = "B17 Rm 150"
+};
+
+Student stu1 = new()
+{
+    FirstName = "Homer",
+    LastName = "Simpson",
+    DateOfBirth = new DateOnly(2000, 3, 20),
+    AssignedAdvisor = compSciAdvisor,
+    PhoneNumber = "2535895580",
+    SchoolEmail = "Homer.Simpson@students.cptc.edu"
+};
+
+Console.WriteLine($"Student: {stu1.FirstName} has {stu1.AssignedAdvisor.FullName}");
