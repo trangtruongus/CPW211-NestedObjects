a74985903c21aafaa034f3321bdae0feaf6b00d7
trangtruongus <trang.truong.us@icloud.com>
2023-07-17 22:33:31 -0500

    Created student and advisor classes

---
+++ CPW211-NestedObjects/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// The legal first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The legal last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public Advisor AssignedAdvisor { get; set; }
+    }
+}
